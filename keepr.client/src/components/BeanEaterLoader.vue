11b07fc0777733c4d91c5d13b12593be073792ff
Tom <afntwrk.guyrpg@gmail.com>
2021-09-29 22:15:49 -0600

    production build

--- keepr.client/src/components/BeanEaterLoader.vue
+++ keepr.client/src/components/BeanEaterLoader.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row h-100 align-items-center">
       <div class="col text-center">
-        <img src="../assets/img/Bean Eater-1s-200px.svg" alt="loading...">
+        <img src="../assets/img/bean-eater.svg" alt="loading...">
         <h2>Loading...</h2>
       </div>
     </div>
